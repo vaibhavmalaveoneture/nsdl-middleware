00feb9413e5a32ef7b4ac80939e6bd5b585661c1
Sachin Dubey <sachin.dubey@oneture.com>
2025-06-13 17:16:53 +0530

    use enable/disable proxy for otp trigger

--- Helper/SmsHelper.cs
+++ Helper/SmsHelper.cs
@@ -17,16 +17,34 @@ namespace ReverseProxyDemo.Helper
 
         public async Task<bool> SendOtpSmsAsync(string phoneno, string otp, string message)
         {
-            string url = string.Empty;
+            string url = string.Empty, ProxyAddress = string.Empty, UseProxy = string.Empty, ProxyUserName=string.Empty, ProxyPassword = string.Empty, ProxyDomain=string.Empty;
             try
             {
-                using (HttpClient client = new HttpClient())
+                url = _configuration["SmsSetting:endpoint"]; // Replace with your URL
+                ProxyAddress = _configuration["SmsSetting:ProxyIP"] + ":" + _configuration["SmsSetting:ProxyPort"];
+                UseProxy = _configuration["SmsSetting:UseProxy"];
+                ProxyUserName = _configuration["SmsSetting:ProxyUserName"];
+                ProxyPassword = _configuration["SmsSetting:ProxyPassword"];
+                ProxyDomain = _configuration["SmsSetting:ProxyDomain"];
+                if (string.IsNullOrEmpty(url))
+                    throw new Exception("no sms endpoint found");
+                url = url.Replace("@Phoneno", phoneno);
+                url = url.Replace("@otp", otp);
+                var handler = new HttpClientHandler();
+                if (UseProxy=="Y")
                 {
-                    url = _configuration["SmsSetting:endpoint"]; // Replace with your URL
-                    if (string.IsNullOrEmpty(url))
-                        throw new Exception("no sms endpoint found");
-                    url=url.Replace("@Phoneno", phoneno);
-                    url=url.Replace("@otp", otp);
+                    handler.Proxy = new WebProxy(ProxyAddress)
+                    {
+                        Credentials = new NetworkCredential(ProxyUserName, ProxyPassword,ProxyDomain)
+                    };
+                    handler.UseProxy = true;
+                }
+                else
+                {
+                    handler.UseProxy = false;
+                }
+                using (HttpClient client = new HttpClient(handler))
+                {
                     HttpResponseMessage response = await client.GetAsync(url);
                     response.EnsureSuccessStatusCode(); // Throws exception for 4xx/5xx
                     string responseBody = await response.Content.ReadAsStringAsync();
