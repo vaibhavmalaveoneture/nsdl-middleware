7ff0c6c86be9f457ba58658193602b77c38238d5
Sachin Dubey <sachin.dubey@oneture.com>
2025-06-30 15:41:16 +0530

    Middleware Reset Password PDF
    (cherry picked from commit 5ec4999f7304ee6feb6b8de5aef3b32609badeeb)

--- Interfaces/IEmailHelper.cs
+++ Interfaces/IEmailHelper.cs
@@ -3,5 +3,6 @@ namespace NSDL.Middleware.Interfaces
     public interface IEmailHelper
     {
         Task<bool> SendOtpEmailAsync(string email, string otp, string purpose);
+        Task<bool> SendEncryptedPdfEmailAsync(string base64Data,string email,string purpose);
     }
 }
